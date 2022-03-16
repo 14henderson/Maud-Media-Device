93cf5a9846fd109399310b4ae94a36382f254a33
UEA\xjh16zfu <xjh16zfu@uea.ac.uk>
2022-03-16 17:25:21 +0000

    More Styling

--- server.js
+++ server.js
@@ -3,31 +3,34 @@ const app = express();
 app.set('view engine', 'ejs');
 const fs = require('fs');
 const path = require('path');
-
+const videosDir = './public/videos';
 const port = process.env.PORT || 8000;
 
 app.use(express.static(path.join(__dirname, '/public/')));
 
+
+function getMediaData(){
+  fs.readdir(videosDir, (err, files) => {
+        console.log(files);
+    });
+}
+
+
+
+
+
+
+
+
+
 app.get('/', function(req, res){
-
-
-
-
-
-
-    res.render('index', {test:'hello world!'});
+  getMediaData();
+  res.render('index', {test:'hello world!'});
 });
 
 
 
 
-
-
-//const videosDir = './public/videos';
-
-    //fs.readdir(videosDir, (err, files) => {
-    //    console.log(files);
-    //});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
