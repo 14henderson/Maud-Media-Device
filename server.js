ff155e3781b604b77433be4332b04b38dcffbafd
14henderson <niklas.henderson@btinternet.com>
2022-04-05 15:50:32 +0100

    front end javascript

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const port = process.env.PORT || 8000;
 app.use(express.static(path.join(__dirname, '/public/')));
 
 const media = {
-    'photos':[],
-    'video':[]
+    'photos':{},
+    'video':{}
 };
 
 function populateMedia(){
@@ -23,45 +23,46 @@ function populateMedia(){
                 data = fs.readFileSync(path.join(__dirname, '/public/descriptions/', path.parse(filename).name+'.txt'), 'utf8');
             } catch (err) {}
             
-            media.video.push({
+            media.video[path.parse(filename).name] = {
+                'type':'video',
                 'title':path.parse(filename).name,
                 'subtitle':data,
-                'filename': path.join('./public/videos/', filename)
-            });
+                'filename': path.normalize(path.join('videos/', filename))
+            };
         });
     });
 
+    //Get photo names and file paths
     fs.readdir(path.join(__dirname, picturesDir), { withFileTypes: true }, function(error, folders){
         folders.forEach((fName) => {
             let parsedFolder = fName.name;
             fs.readdir(path.join(__dirname, picturesDir, parsedFolder), function(error, folders){
-                media.photos.push({
+                media.photos[parsedFolder] = {
+                    'type':'album',
                     'title': parsedFolder,
-                    'albumPath': path.join('albums', parsedFolder),
+                    'albumPath': path.normalize(path.join('albums', parsedFolder)),
                     'imagePaths': folders
-                });
+                };
             });
         });
     });
-
-
-
-
-
 }
-
-
 
 populateMedia();
 
 
-
-
 app.get('/', function(req, res){
-    res.render('index', {'media':media});
+    let data = Object.values(media.photos).concat(Object.values(media.video));
+    res.render('index', {'media':data});
 });
 
-
+app.get('/albums/:albumName', function(req, res){
+    let albumName = req.params.albumName;
+    
+    let albumData = media.photos[albumName];
+    console.log(albumData);
+    res.render('album', {'data':albumData});
+})
 
 
 
